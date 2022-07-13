4ce1c150e65fb1d227485ba0fef45670f6b3dfb9
Ramon Duque <ramonduke@gmail.com>
2022-07-13 12:50:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+ function getAreaOfCircle(radius){
+    return (radius * radius * Math.PI);
+ }
+
+console.log(getAreaOfCircle(5));
+
+function getCircumferenceOfCircle (radius){
+  return (2 * Math.PI * radius);
+}
+
+console.log(getCircumferenceOfCircle(8))
+
+function getAreaOfSquare (side){
+  return (side * 4);
+}
+
+console.log (getAreaOfSquare(5));
+
+function getAreaOfTriange (base, height){
+  return (base * height) / 2;
+}
+
+console.log(getAreaOfTriange(2, 4));
+
+
+
+
+
+
+
